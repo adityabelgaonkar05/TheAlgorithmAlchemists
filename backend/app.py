99267997134c59eb16d69a114ca2557bc125cf32
rjdp <78971526+r9jdp@users.noreply.github.com>
2024-02-03 14:55:38 +0530

    removed the /home link

--- backend/app.py
+++ backend/app.py
@@ -5,12 +5,14 @@ app = Flask(__name__)
 cors = CORS(app)
 app.config['CORS_HEADERS'] = 'Content-Type'
 
-@app.route("/home", methods=["POST","GET"])
+@app.route("/", methods=["POST","GET"])
 @cross_origin()
 def home():
     if request.method == "GET":
         return render_template("new.html")
     if request.method == 'POST':
+        for key in request.form:
+            print(key)
         return {"username" : f"{request.form['username']}","password" :f"{request.form['password']}" }
 
 
